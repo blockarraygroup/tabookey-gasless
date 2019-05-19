2ea8a4f15aa7f228214be778beaa5943e5a51ab1
shahafn <shahaf@tabookey.com>
2019-05-19 17:13:16 +0300

    Adding missing error handling

--- server/src/librelay/relay_server.go
+++ server/src/librelay/relay_server.go
@@ -503,12 +503,17 @@ func (relay *relayServer) CreateRelayTransaction(request RelayTransactionRequest
 		From: request.From,
 		Data: input,
 	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	maxCharge := request.GasPrice.Uint64() * gasEstimate * (100 + relay.GasPricePercent.Uint64()) / 100
 	if toBalance.Uint64() < maxCharge {
 		err = fmt.Errorf("Recipient balance too low: %d, gasEstimate*fee: %d", toBalance, maxCharge)
 		log.Println(err)
 		return
 	}
+	log.Println("Estimated max charge of relyed tx:",maxCharge)
 
 	nonceMutex.Lock()
 	defer nonceMutex.Unlock()
